5d0022618f590d8f6c74bac0d7102929e3c6f2e2
Theus <153127255+Math-Porfiro@users.noreply.github.com>
2023-12-21 02:10:51 -0300

    Update desafio2.js

--- desafio2.js
+++ desafio2.js
@@ -1,1 +1,42 @@
+// Desafio DIO (Java Script)
 
+// Função para calcular o nível do jogador
+function calcularNivel(vitorias, derrotas) {
+    // Calcula o saldo de vitórias
+    const saldoVitorias = vitorias - derrotas;
+
+    // Determina o nível com base no saldo de vitórias
+    let nivel;
+    if (saldoVitorias <= 10) {
+        nivel = "Ferro"
+    } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+        nivel = "Bronze"
+    } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+        nivel = "Prata"
+    } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+        nivel = "Ouro"
+    } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+        nivel = "Diamante"
+    } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+        nivel = "Lendário"
+    } else if (saldoVitorias >= 101) {
+        nivel = "Imortal"
+    } else {
+        nivel = "Nível não identificado"
+    }
+
+    // Retorna o resultado
+    return {
+        saldoVitorias,
+        nivel
+    }
+}
+
+// Numere seu placar de vitórias e derrotas
+let vitorias = 95
+let derrotas = 17
+
+const resultado = calcularNivel(vitorias, derrotas)
+
+// Mensagem exibida ao final
+console.log(`O Herói tem um saldo de ${resultado.saldoVitorias} vitórias e está no nível ${resultado.nivel}`)
